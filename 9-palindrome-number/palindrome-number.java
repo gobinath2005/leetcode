f9d3f3d52e5c2d34bd3b0a2aa190805bb3d22567
gobinath2005 <gobinathdhandapani1712@gmail.com>
2026-06-16 18:38:21 +0530

    Time: 26 ms (5.91%) | Memory: 46.2 MB (17.27%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int rev=0;
+        int temp=x;
+        while(x>0){
+            int rem=x%10;
+            rev=rev*10+rem;
+            x=x/10;
+            
+        }
+        if(temp==rev){
+            System.out.println("ture");
+        }
+        else{
+        System.out.println("false");
+        }
+
+      return temp==rev;
+    }
+}
